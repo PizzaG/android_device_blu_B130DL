3c28ebe2c23bbfad4f301741090837fef5cd3ed1
PizzaG <49663808+PizzaG@users.noreply.github.com>
2023-01-26 08:42:46 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 The TWRP Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 # Copyright (C) 2019-Present A-Team Digital Solutions
 #
 
@@ -17,7 +17,7 @@ TARGET_CPU_VARIANT := generic
 # Assert
 TARGET_OTA_ASSERT_DEVICE := B130DL
 
-# File systems
+# File Systems
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
@@ -60,7 +60,7 @@ BOARD_HAS_MTK_HARDWARE := true
 # Platform
 TARGET_BOARD_PLATFORM := mt6761
 
-# Hack: prevent anti rollback
+# Prevent Anti-Rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
@@ -72,9 +72,9 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 
-# Build Flags
+# TWRP Build Flags
 TW_MAINTAINER := PizzaG
 TW_DEVICE_VERSION := View_2
-RECOVERY_VARIANT := TWRP_10
+RECOVERY_VARIANT := TWRP_12.1
 ALLOW_MISSING_DEPENDENCIES := true
 LC_ALL := "C"
