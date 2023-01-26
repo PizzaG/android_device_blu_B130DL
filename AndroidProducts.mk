3e7a43fe6b59a20069d3ace1260f3c10aa329248
Kalibyr <56097364+kalibyr-l33t@users.noreply.github.com>
2023-01-26 07:30:11 -0600

    Fixed formatting

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,7 @@
-#
 # Copyright (C) 2020 The Android Open Source Project
 # Copyright (C) 2020 The TWRP Open Source Project
 # Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 # Copyright (C) 2019-Present A-Team Digital Solutions
-#
 
 PRODUCT_MAKEFILES := \
      $(LOCAL_DIR)/twrp_B130DL.mk
