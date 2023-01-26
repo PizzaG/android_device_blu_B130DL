1ef2c8fbccdfa645ff3210ad13307c6d09947c81
PizzaG <49663808+PizzaG@users.noreply.github.com>
2023-01-26 08:43:23 -0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,7 +1,7 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 The TWRP Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 # Copyright (C) 2019-Present A-Team Digital Solutions
 #
 
