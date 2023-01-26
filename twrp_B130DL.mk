f3e97bd67c98b16448be7401290a50cd5bbe96bf
Kalibyr <56097364+kalibyr-l33t@users.noreply.github.com>
2023-01-26 07:52:24 -0600

    Code formatting

--- twrp_B130DL.mk
+++ twrp_B130DL.mk
@@ -1,9 +1,7 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
 # Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 # Copyright (C) 2019-Present A-Team Digital Solutions
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
@@ -19,10 +17,10 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/blu/B130DL/recovery/root,recovery/root)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := B130DL
-PRODUCT_NAME := twrp_B130DL
-PRODUCT_BRAND := View
-PRODUCT_MODEL := B130DL
+PRODUCT_DEVICE  := B130DL
+PRODUCT_NAME    := twrp_B130DL
+PRODUCT_BRAND   := View
+PRODUCT_MODEL   := B130DL
 PRODUCT_MANUFACTURER := Blu
 PRODUCT_RELEASE_NAME := Blu View 2
 
