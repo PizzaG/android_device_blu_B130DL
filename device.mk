b5918f64ce9b7b8254bc21fdfcf1419e45667d46
Kalibyr <56097364+kalibyr-l33t@users.noreply.github.com>
2023-01-26 06:11:01 -0600

    Correct me if I'm wrong...
    I'mma just
     - Fix a capital
     - Update Copyright
     - Introduce a new way for you to make block comments??
     - Start a list of things to refine

--- device.mk
+++ device.mk
@@ -1,17 +1,16 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-# Copyright (C) 2019-Present A-Team Digital Solutions
-#
+define Copyright
+Copyright (C) 2022 The Android Open Source Project
+Copyright (C) 2022 The TWRP Open Source Project
+Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+Copyright (C) 2019-Present A-Team Digital Solutions
+endef
 
 LOCAL_PATH := device/blu/B130DL
 
-# Dynamic partitions
+# Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-
