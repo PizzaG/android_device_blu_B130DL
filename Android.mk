b4f0581efa0368b740dacfdb9dcaffa2d9a3cfb5
PizzaG <49663808+PizzaG@users.noreply.github.com>
2023-01-26 08:39:51 -0600

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,8 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The TWRP Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2019-Present A-Team Digital Solutions
 #
 
 LOCAL_PATH := $(call my-dir)
