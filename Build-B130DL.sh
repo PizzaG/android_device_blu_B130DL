55f6e33a78d0094f39b20b6b8e6096b9fa60568d
PizzaG <49663808+PizzaG@users.noreply.github.com>
2023-02-02 11:18:13 -0600

    fix for building

--- Build-B130DL.sh
+++ Build-B130DL.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-make clean
+## TWRP
+#############################
+export USE_CCACHE=1
 . build/envsetup.sh
+m clean
 lunch twrp_B130DL-eng
 mka recoveryimage
-
+echo
 echo " Recovery Should Be Built"
 echo ""
 read
+#############################
+#############################
+
